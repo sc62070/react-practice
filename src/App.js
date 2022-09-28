6d251eaf7050907dd9ea2bdcc100ab0680587cc5
Hyuk Jin Choi <sc62070@gmail.com>
2022-09-28 21:30:57 +0900

    app.css added, search svg added, app js modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
+//import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
+const API_URL = 'http://www.omdbapi.com?apikey=e8452209'
 
 const App = () => {
-    return (
-        <h1>App</h1>
-    );
-}
+  
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
 
-export default App;
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('superman');
+    }, []);
+    
+    return(
+        <div className="app">
+            <h1>김정효 잘잔다zzzzzz</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value="Superman"
+                    onChange={()=>{}}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => {}}
+                />
+            </div>
+            <
+
+
+
+
+
+        </div>
+    );
+    
+}
+  
+  export default App;
