35a2c6bf64335265ca9f0ac48c4567509008deec
Hyuk Jin Choi <sc62070@gmail.com>
2022-09-28 22:02:01 +0900

    김정효 계속잔다...zzZZ

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   
     return (
       <div className="app">
-        <h1>MovieLand</h1>
+        <h1>김정효 계속잔다..zzZ</h1>
   
         <div className="search">
           <input
